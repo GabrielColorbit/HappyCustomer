4ad419f09ccfbd15b9dd75fdac09353578e7a84e
guirocky <guilhermesantosgraminho@gmail.com>
2016-10-26 11:09:44 -0200

    ajudando

--- src/br/edu/facear/crm/dao/Connection.java
+++ src/br/edu/facear/crm/dao/Connection.java
@@ -7,7 +7,7 @@ import javax.persistence.Persistence;
 public class Connection {
 	static EntityManagerFactory emf = 
 			Persistence.createEntityManagerFactory("CRM_Feliz");
-	
+	//consegui arrumar aqui, agora está funcionando 100 por cento.
 	public static EntityManager getEntityManager() {
 		return emf.createEntityManager();
 	}
