4b0c1856a51920063b307cc09ca0a371c8492191
GabrielColorbit <gabriel@colorbit.com.br>
2016-11-05 00:48:32 -0200

    Tipocontato controller q faltou

--- WebContent/tipo_contato/TipoContatoController.js
+++ WebContent/tipo_contato/TipoContatoController.js
@@ -1,6 +1,7 @@
 var myControllers = angular.module('TipoContatoControllers',[]);
 
 myControllers.controller('ListarTipoContatoController', function($scope,$http) {
+<<<<<<< HEAD
 	$scope.Titulo = "Tipos de Contato";
 	$scope.tipoContatolist = [];
 	$scope.BuscarInformacao = function() {
@@ -12,9 +13,18 @@ myControllers.controller('ListarTipoContatoController', function($scope,$http) {
 			}
 			
 			
+=======
+	$scope.Titulo = "Tipos de Contatos";
+	$scope.BuscarInformacao = function() {
+		$http.get('http://localhost:8080/CRM/rest/restTipoContato/listarTodos')
+		.success(function(data) {
+			$scope.tipoContatolist = data["tipoContato"];
+			$scope.Quantidade = $scope.tipoContatolist.length+' Tipos de Contatos Encontrados!' ;
+>>>>>>> origin/master
 		});
 	};
 	$scope.BuscarInformacao();
+	
 	$scope.ordenar = function(keyname){
         $scope.sortKey = keyname;
         $scope.reverse = !$scope.reverse;
@@ -43,7 +53,7 @@ myControllers.controller('TipoContatoController', function($scope, $routeParams,
 	$scope.EnviarInformacao = function() {
 		
 		var parameter = JSON.stringify({
-			type : "tipoContato",
+			type : "origemContato",
 			id : $scope.tipoContato.id,
 			nome : $scope.tipoContato.nome
 		});
@@ -57,7 +67,7 @@ myControllers.controller('TipoContatoController', function($scope, $routeParams,
 				'http://localhost:8080/CRM/rest/restTipoContato/Salvar',
 				parameter, config).success(
 				function(data, status, headers, config) {
-					$scope.Resposta = 'Tipo Contato Salvo com Sucesso!';
+					$scope.Resposta = 'TipoContato ('+$scope.tipoContato.nome+') Salvo com Sucesso!';
 					
 					
 				}).error(
@@ -65,5 +75,21 @@ myControllers.controller('TipoContatoController', function($scope, $routeParams,
 					$scope.Resposta = data ;
 				});
 	   };
+	   $scope.Excluir = function(id){
+		   if(id){
+				
+				$http.post('http://localhost:8080/CRM/rest/restTipoContato/Excluir/'+id)
+					.success(
+					function(data, status) {
+						$scope.Resposta = 'Tipo de Contato Excluído com Sucesso!';
+						$scope.BuscarInformacao();
+						
+					}).error(
+					function(data, status) {
+						$scope.Resposta = data ;
+					});
+			   };
+			
+			};
 	
 });
