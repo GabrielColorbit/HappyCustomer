ac706f3af1a4a2bd7fe1bb0bec3df8feda239a1f
GabrielColorbit <gabriel@colorbit.com.br>
2016-12-29 10:36:38 -0200

    Correções gráfico de atividades por empresa
    Correções em amostragem de meses desconfigurada por conta de falta de
    um subselect no script de consulta no database.

--- src/files/AtividadePorEmpresaMedia.sql
+++ src/files/AtividadePorEmpresaMedia.sql
@@ -1,103 +1,97 @@
-with
-totalempresa as (
-    select
-    	count(ef.empresa) as empresa
-    	,ef.mes as mes
-    from(
-        select
-            sum(a.empresa_id) as total
-            ,a.empresa_id as empresa
-            ,case EXTRACT(month from (to_date(a.datacadastro, 'DD/MM/YYYY') ))
-                when 1 then 'Janeiro'
-                when 2 then 'Fevereiro'
-                when 3 then 'Março'
-                when 4 then 'Abril'
-                when 5 then 'Maio'
-                when 6 then 'Junho'
-                when 7 then 'Julho'
-                when 8 then 'Agosto'
-                when 9 then 'Setembro'
-                when 10 then 'Outubro'
-                when 11 then 'Novembro'
-                when 12 then 'Dezembro'
-                else 'Indefinido'
-            end AS mes
+with totalempresa as(
+  Select count(ef.empresa) as empresa
+  ,ef.mes as mes from(
+      select
+        a.empresa_id as empresa
+        ,case EXTRACT(month from (to_date(a.datacadastro, 'DD/MM/YYYY')))
+          when 1 then '01-Janeiro'
+          when 2 then '02-Fevereiro'
+          when 3 then '03-Marco'
+          when 4 then '04-Abril'
+          when 5 then '05-Maio'
+          when 6 then '06-Junho'
+          when 7 then '07-Julho'
+          when 8 then '08-Agosto'
+          when 9 then '09-Setembro'
+          when 10 then '10-Outubro'
+          when 11 then '11-Novembro'
+          when 12 then '12-Dezembro'
+          else 'Indefinido'
+          end AS mes
+      From \"TB_ATIVIDADE\" a
+      Group By a.datacadastro
+      ,a.empresa_id
+  )as ef
+  Group by ef.mes
+  Order by ef.mes
 
-            From "TB_ATIVIDADE" a
-            Group By
-            a.datacadastro
-            ,a.empresa_id
-    )as ef
-    Group By
-      ef.mes
-),
-totalatividade as (
- 	select
-        count(a.id) as atividade
-		,case EXTRACT(month from (to_date(a.datacadastro, 'DD/MM/YYYY') ))
-            when 1 then 'Janeiro'
-            when 2 then 'Fevereiro'
-            when 3 then 'Março'
-            when 4 then 'Abril'
-            when 5 then 'Maio'
-            when 6 then 'Junho'
-            when 7 then 'Julho'
-            when 8 then 'Agosto'
-            when 9 then 'Setembro'
-            when 10 then 'Outubro'
-            when 11 then 'Novembro'
-            when 12 then 'Dezembro'
-            else 'Indefinido'
-         end   AS mes
-
-    From "TB_ATIVIDADE" a
-    Group by
-       a.datacadastro
-
-),
-media as (
-	select
-    	round( ta.atividade::numeric, 4)/round( te.empresa::numeric, 4) as media
-    	,te.mes
-	from totalempresa te
-    inner join totalatividade ta on (ta.mes = te.mes)
-
-)
-,atividades as (
-    select
-
-        count(a.id) as numeratividade
-       ,case EXTRACT(month from (to_date(a.datacadastro, 'DD/MM/YYYY') ))
-            when 1 then 'Janeiro'
-            when 2 then 'Fevereiro'
-            when 3 then 'Março'
-            when 4 then 'Abril'
-            when 5 then 'Maio'
-            when 6 then 'Junho'
-            when 7 then 'Julho'
-            when 8 then 'Agosto'
-            when 9 then 'Setembro'
-            when 10 then 'Outubro'
-            when 11 then 'Novembro'
-            when 12 then 'Dezembro'
-            else 'Indefinido'
-         end   AS mes
-
-    From "TB_ATIVIDADE"  a
-    Where
-        a.empresa_id = 1
-    Group by
-       a.datacadastro
-),
-kpi as (
-    select
-        count(a.numeratividade) as numeroatividade
-    	,m.media
-        ,a.mes
-    From atividades  a
+),totalatividade as (
+  Select count(ef.atividade) as atividade
+    ,ef.mes as mes
+  From(select
+        a.id as atividade
+        ,case EXTRACT(month from (to_date(a.datacadastro, 'DD/MM/YYYY')))
+          when 1 then '01-Janeiro'
+          when 2 then '02-Fevereiro'
+          when 3 then '03-Marco'
+          when 4 then '04-Abril'
+          when 5 then '05-Maio'
+          when 6 then '06-Junho'
+          when 7 then '07-Julho'
+          when 8 then '08-Agosto'
+          when 9 then '09-Setembro'
+          when 10 then '10-Outubro'
+          when 11 then '11-Novembro'
+          when 12 then '12-Dezembro'
+          else 'Indefinido'
+          end AS mes
+      from \"TB_ATIVIDADE\" a
+      group By
+      a.datacadastro
+      ,a.id
+  )as ef
+  Group by ef.mes
+  Order by ef.mes
+), media as (
+  Select
+    round( ta.atividade::numeric, 4)/round( te.empresa::numeric, 4) as media
+    ,te.mes from totalempresa te inner join totalatividade ta on (ta.mes = te.mes)
+), atividades as (
+  Select
+     count(ef.atividade) as numeroatividade
+     ,ef.mes as mes
+  From(select
+       a.id as atividade
+       ,case EXTRACT(month from (to_date(a.datacadastro, 'DD/MM/YYYY')))
+         when 1 then '01-Janeiro'
+         when 2 then '02-Fevereiro'
+         when 3 then '03-Marco'
+         when 4 then '04-Abril'
+         when 5 then '05-Maio'
+         when 6 then '06-Junho'
+         when 7 then '07-Julho'
+         when 8 then '08-Agosto'
+         when 9 then '09-Setembro'
+         when 10 then '10-Outubro'
+         when 11 then '11-Novembro'
+         when 12 then '12-Dezembro'
+         else 'Indefinido'
+        end AS mes
+     from \"TB_ATIVIDADE\" a
+     where a.empresa_id = "+idempresa+"
+     group By
+     a.datacadastro
+     ,a.id
+   )as ef
+   Group by ef.mes
+   Order by ef.mes
+), kpi as (
+    Select
+      a.numeroatividade
+      ,m.media
+      ,a.mes
+    From atividades a
     inner join media m on (m.mes = a.mes)
-	Group By
-        m.media
-        ,a.mes
-
-)select * from kpi
+    Order by a.mes
+)
+Select * from kpi
